4367974dfd198a376b4fd03facf12067a179bdb7
Predrag Vuckovic <predrag.vuckovic80@outlook.com>
2025-02-14 17:23:57 +0100

    test connection

--- wp-content/themes/twentytwentyfive/patterns/header.php
+++ wp-content/themes/twentytwentyfive/patterns/header.php
@@ -13,7 +13,7 @@
 
 ?>
 <!-- wp:group {"align":"full","layout":{"type":"default"}} -->
-<div class="wp-block-group alignfull">scscsc
+<div class="wp-block-group alignfull">scscscscsc
 	<!-- wp:group {"layout":{"type":"constrained"}} -->
 	<div class="wp-block-group">
 		<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
